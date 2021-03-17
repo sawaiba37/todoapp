35bd08d3cbf646438613b05eaffcda3cc57d1a6d
abc <you@example.com>
2021-03-17 14:21:57 +0500

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,112 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+  
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+      <div class="text-h3">Todo</div>
+      <div class="text-subtitles">{{todaysDate}}</div>
+      </div>
+
+      <q-img
+        src="icons/mountain.jpg"
+     class="header-image absolute-top" />
+      
+    </q-header>
+
+    <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 185px); margin-top: 185px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item 
+            to="/"
+            exact 
+            clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Todo        
+                 </q-item-section>
+            </q-item>
+
+            <q-item 
+            to="/help"
+            exact
+            clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+            Help
+              </q-item-section>
+            </q-item>
+
+            
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="icons/draw.jpg" style="height: 185px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="icons/avatar3.jpg">
+            </q-avatar>
+            <div class="text-weight-bold">Sawaiba A.Sattar</div>
+            <div>@S.A.sattar</div>
+          </div>
+        </q-img>
+      </q-drawer>
+    <q-page-container>
+      <keep-alive>
+         <router-view />
+      </keep-alive>
+     
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+ import { date } from 'quasar'
+
+export default {
+ 
+ 
+  data () {
+    return {
+      leftDrawerOpen: false,
+     
+    }
+  },
+  computed:{
+    todaysDate(){
+      let timeStamp = Date.now()
+return date.formatDate(timeStamp, 'dddd, D MMM YYYY')
+    }
+  }
+}
+</script>
+<style lang="scss">
+   .header-image{
+  height:100%;
+  z-index:-1;
+  opacity:0.2;
+  filter:grayscale(100%);
+}
+</style>
---
+++ src/pages/help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p><b><h4>Todo App</h4></b></p>
+  <p>A truly cross platform task manager. Whether you're at home or on the go you can access your task list and stay organized</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
